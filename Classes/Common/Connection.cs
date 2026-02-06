7cb609a5863c9c5feaa02c3776aaf461751220d8
kenn1d <makarkibanov220@gmail.com>
2026-02-06 23:12:23 +0500

    Разработанные контексты данных для работы с БД. Кибанов.

--- Classes/Common/Connection.cs
+++ Classes/Common/Connection.cs
@@ -1,10 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace praktika20.Classes.Common
 {
