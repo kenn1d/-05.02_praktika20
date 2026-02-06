0b348f1bc175b68797920be213c1e68ede286376
kenn1d <makarkibanov220@gmail.com>
2026-02-07 00:13:03 +0500

    Программирование окна MainWindow. Кибанов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,18 @@ namespace praktika20
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+
+            OpenPages(new Pages.Main());
+        }
+
+        public void OpenPages(Page page)
+        {
+            frame.Navigate(page);
         }
     }
 }
