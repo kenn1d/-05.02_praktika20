c246767cd7fd4cf23d464bf728e7c9463155c097
kenn1d <makarkibanov220@gmail.com>
2026-02-06 22:43:29 +0500

    Разработанный класс подключения к БД. Кибанов

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika20.Classes.Common
+{
+    public class Connection
+    {
+        public static string config = "server=localhost;uid=root;database=journal;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
